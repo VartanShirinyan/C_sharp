8385729882f837b6495d6f4c654907c707f4658f
Vartan <02vartan-s@mail.ru>
2022-11-28 17:33:47 +0300

    Напишите программу, которая принимает на вход целое число любой разрядности и на выходе показывает третью цифру слева этого числа или говорит, что такой цифры нет

---
+++ C#_Seminar1/5_work/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter an integer number (Введите целое число)");
+int number = Convert.ToInt32(Console.ReadLine());
+int d = 0, k = 0;
+if (number > 99)
+{
+    k = number;
+    while (k >= 1000)
+    {
+        k /= 10;
+        d = k % 10;
+    }
+Console.WriteLine($"The third number from the left is {d}");
+}
+else
+{
+    Console.WriteLine($"The number has fewer then 3 digits, sorry.");
+}
