3f4dc66bf51535c522517f46b4f08c2dbe454b17
Vartan <02vartan-s@mail.ru>
2022-11-28 16:51:48 +0300

    4_work

---
+++ C#_Seminar1/4_work/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Enter a three-digit integer number (Введите трехзначное число)");
+int number = Convert.ToInt32(Console.ReadLine());
+int last_digit = number%10;
+Console.WriteLine($"Last digit of your number is(последняя цифра вашего номера) {last_digit}");
