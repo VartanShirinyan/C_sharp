36bac69f2946735c7bf8ab87f98fea66543ac536
Vartan <02vartan-s@mail.ru>
2022-11-28 16:27:34 +0300

    3_work

---
+++ C#_Seminar1/3_work/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter a positive integer(введите положительное целое число)");
+int n = Convert.ToInt32(Console.ReadLine());
+int k = -n;
+while (k<n+1)
+{
+    Console.Write($"{k} ");
+    k++;
+}
+Console.WriteLine();
