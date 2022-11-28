8dfce63b5ef13a82d13f38a49aeef7800e937557
Vartan <02vartan-s@mail.ru>
2022-11-28 15:58:16 +0300

    первая групповая задача

---
+++ C#_Seminar1/2_work/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine(value: "Please, input the first integer number!");
+int n1 = Convert.ToInt32(value: Console.ReadLine());
+Console.WriteLine(value: "Please, input the second integer number different from the first!");
+int n2 = Convert.ToInt32(value: Console.ReadLine());
+
+if (n1 == n2 * n2)
+{
+    Console.WriteLine(value: "Первое число является квадратом второго!");
+}
+else if (n2 == n1 * n1)
+{
+    Console.WriteLine(value: "Второе число является квадратом Первого!");
+}
+else
+{
+    Console.WriteLine(value: "Квадраты чисел отсутствуют!");
+}
