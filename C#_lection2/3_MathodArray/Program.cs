cc8ceb9996f4954d210ad53dd087b53d2b2c6206
Vartan Shirinyan <02vartan-s@mail.ru>
2022-12-02 16:15:49 +0300

    добавил MethodArray

---
+++ C#_lection2/3_MathodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 24, 45, 14, 45, 56, 45, 78 };
+
+int n = array.Length;
+int find = 45;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;    
+    }
+    // index = index + 1;
+    index++;
+}
